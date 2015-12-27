1b09d4fbefd992514926208bebae4b2d01e4f156
tuancuongth88 <tuancuongth88@gmail.com>
2015-12-27 13:39:01 +0700

    add project

---
+++ Project_datsan/Project_datsan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_datsan.Startup))]
+namespace Project_datsan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
